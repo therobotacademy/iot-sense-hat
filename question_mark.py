93b1eba02769c775b89ac73efee6c12fb7a52797
brjapon <brjapon@therobotacademy.com>
2018-08-16 14:41:45 +0200

    Question mark red over blue background

--- question_mark.py
+++ question_mark.py
@@ -3,7 +3,7 @@ import time
 from sense_hat import SenseHat
 sense = SenseHat()
 X = [255, 0, 0] # Red
-o = [255, 255, 255] # White
+o = [0, 0, 150] # Blue
 
 question_mark = [
 o, o, o, X, X, o, o, o,
