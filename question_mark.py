a5550f401f0bf528b5f2798962c9f7bec9f6a193
brjapon <bernardo.ronquillo@alestis.aero>
2018-08-16 14:35:03 +0200

    Mostrar una interrogación durante 2 segundos

---
+++ question_mark.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import time
+from sense_hat import SenseHat
+sense = SenseHat()
+X = [255, 0, 0] # Red
+o = [255, 255, 255] # White
+
+question_mark = [
+o, o, o, X, X, o, o, o,
+o, o, X, o, o, X, o, o,
+o, o, o, o, o, X, o, o,
+o, o, o, o, X, o, o, o,
+o, o, o, X, o, o, o, o,
+o, o, o, X, o, o, o, o,
+o, o, o, o, o, o, o, o,
+o, o, o, X, o, o, o, o
+]
+sense.set_pixels(question_mark)
+time.sleep(2)
+sense.clear()
